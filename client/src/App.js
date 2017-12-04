ca8b2bb9b36de85c2339b6f8743ca041ec191fdb
Kelly Grenz <kelly@witcreative-studio.com>
2017-12-04 11:18:11 -0700

    app up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react'
+// import Header from './Header'
 
-const App = () => <div> Hello World 👋 </div>
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello Full Stack World</h1>
+      </div>
+    )
+  }
+}
+  
+
+
 
 
 export default App
+
